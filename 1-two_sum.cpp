5cd2bd0f830e680f1e73d68d267e94e623e73aab
risyomei <risyomei@gmail.com>
2020-01-09 00:03:09 +0900

    1

---
+++ 1-two_sum.cpp
@@ -0,0 +1,46 @@
+struct Data {
+    int index;
+    int value;
+};
+
+class Solution {
+public:
+    
+    vector<int> twoSum(vector<int>& nums, int target) {
+        
+        vector<int> answer;
+        
+        vector<Data> dataArray;
+        
+        for( int i = 0; i < nums.size(); i++) {
+            Data temp;
+            temp.index = i;
+            temp.value = nums[i];         
+            dataArray.push_back(temp);
+        }
+        
+        
+        sort( dataArray.begin(), dataArray.end(), [](Data a, Data b) {
+            return a.value < b.value ;   
+        });
+        
+        int i = 0, j = nums.size() -1;
+        
+        do{
+            if( dataArray[i].value + dataArray[j].value == target){
+                answer.push_back( dataArray[i].index );
+                answer.push_back( dataArray[j].index );
+                return answer;
+                
+            }else if( dataArray[i].value + dataArray[j].value < target ){
+                i++;
+            }else {
+                j--;
+            }
+            
+            
+        } while  ( i < j);
+        
+        return answer;
+    }
+};
